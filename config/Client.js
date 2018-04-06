12537121f10ba3fcc736adf3423bf57bdfa8a875
Paul Desmond Parker <pauldesmondparker@gmail.com>
2018-04-06 15:34:58 +0800

    Add auth headers to graphql requests

---
+++ config/Client.js
@@ -0,0 +1,55 @@
+import ApolloClient from 'apollo-boost';
+
+export default client = new ApolloClient({
+  uri: 'http://notify.parker.sg:3000/graphql',
+  fetchOptions: {
+    credentials: 'include'
+  },
+  request: async (operation) => {
+    const token = await Expo.SecureStore.getItemAsync('token');
+    //const { cache } = operation.getContext();
+    //const { isLogged } = cache.readQuery({ data: { isLogged }});
+/*    if ( isLogged ) { */
+    operation.setContext({
+      headers: {
+        authorization: 'Bearer ' + token
+      }
+    });
+/*    } else {
+      operation.setContext({
+        headers: {
+          authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyaWQiOiIiLCJyb2xlIjpbeyJuYW1lIjoiR0VORVJBTCJ9XSwiaWF0IjoxNTIyOTg0NDUzfQ.GK3Mrk1CQmcdEnvlFopzS2dnK88c2kOx6PXkm31IkOU'
+        }
+      });
+    } */
+  },
+  onError: ({ operation, graphQLErrors, networkError }) => {
+    const { cache } = operation.getContext();
+    if (graphQLErrors) {
+      console.log( graphQLErrors );
+    }
+    if (networkError) {
+      cache.writeData({ data: { isLogged: true }});
+    }
+  },
+  clientState: {
+    defaults: {
+        isConnected: true
+      , isLogged: false
+    },
+    resolvers: {
+      Mutation: {
+        updateNetworkStatus: (_, { isConnected }, { cache }) => {
+          cache.writeData({ data: { isConnected: isConnected }});
+          return null;
+        }
+      }
+    }
+  },
+  //cacheRedirects: {
+    //Query: {
+      //movie: (_, { id }, { getCacheKey }) =>
+        //getCacheKey({ __typename: 'Movie', id });
+    //}
+  //}
+});
